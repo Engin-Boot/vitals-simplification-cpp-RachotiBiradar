443ee850225909ec2b85633979209574cda7e939
RachotiBiradar <44144000+RachotiBiradar@users.noreply.github.com>
2020-08-27 12:29:24 +0530

    abstaction add

--- checker.cpp
+++ checker.cpp
@@ -1,18 +1,21 @@
 #include <assert.h>
-bool bpm_check(float bpm){
-   return bpm < 70 || bpm > 150;
-}
-bool spo2_check(float spo2){
-return spo2 < 90;
-}
-bool respRate_check(float respRate){
-return respRate < 30 || respRate > 95;  
+
+bool isInRange(float value,int lowerLimit,int upperLimit){
+return (value>=lowerLimit||value<=upperLimit);
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return bpm_check(bpm)&&spo2_check(spo2)&&respRate_check(respRate);
+
+  return (isInRange(bpm,70,150) && isInRange(spo2,90,100) && isInRange(respRate,30,95));
+
 }
+
+
+
 int main() {
+
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
+  assert(vitalsAreOk(60,95,103)==false);
+  assert(vitalsAreOk(80,82,28)==false);
 }
